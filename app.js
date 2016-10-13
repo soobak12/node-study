16d01699c120f8f8a0d9e1f63f7eaee2d33d855f
soobak12 <soobak12@gmail.com>
2016-10-13 16:39:14 +0900

    add package.json

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('yasdf');
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+
+    console.log(`-> ${request.method}${request.url}`);
+    response.end('byebye');
+    // 여기서 헤더, 메소드, url, 바디를 가지게 되었고
+    // 이 요청에 응답하는 데 필요한 어떤 일이라도 할 수 있게 되었습니다.
+  });
+}).listen(8080); // 이 서버를 활성화하고 8080 포트로 받습니다.
