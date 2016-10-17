4831f348c51ee85accdd65f1445b36912df44a66
soobak12 <soobak12@gmail.com>
2016-10-17 10:41:09 +0900

    stable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var os = require('os');
 
 http.createServer(function(request, response) {
   var headers = request.headers;
@@ -13,6 +14,18 @@ http.createServer(function(request, response) {
     body = Buffer.concat(body).toString();
 
     console.log(`-> ${request.method}${request.url}`);
+
+    switch(request.url){
+      case '/cpu':
+        var cpus=JSON.stringify(os.cpus()[0].model); // JSON.stringify:객체를 JSON으로 저장
+        //var first=JSON.stringify(cpus[0]);
+        response.end(cpus);
+        break;
+      default:
+        response.end('not found');
+        break;
+    };
+
     response.end('byebye');
     // 여기서 헤더, 메소드, url, 바디를 가지게 되었고
     // 이 요청에 응답하는 데 필요한 어떤 일이라도 할 수 있게 되었습니다.
